b4a458cf3f98f2a622e2d8a6463f4f284d3de45c
joshuasearle <joshryansearle@gmail.com>
2023-04-26 09:01:59 +1000

    Add neotest

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -81,6 +81,105 @@ return {
   {
     "echasnovski/mini.surround",
     event = { "BufReadPre", "BufNewFile" },
-    opts = {},
+    config = function()
+      require("mini.surround").setup()
+    end,
+  },
+
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "Issafalcon/neotest-dotnet",
+    },
+    keys = {
+      {
+        "<leader>tt",
+        function()
+          require("neotest").run.run()
+        end,
+        desc = "Run closest test",
+      },
+      {
+        "<leader>tf",
+        function()
+          require("neotest").run.run(vim.fn.expand("%"))
+        end,
+        desc = "Test file",
+      },
+      {
+        "<leader>tp",
+        function()
+          require("neotest").run.run({ suite = true })
+        end,
+        desc = "Test project",
+      },
+      {
+        "<leader>tx",
+        function()
+          require("neotest").run.stop()
+        end,
+        desc = "Stop tests",
+      },
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run last test",
+      },
+      {
+        "<leader>tL",
+        function()
+          require("neotest").run.run_last({ strategy = "dap" })
+        end,
+        desc = "Run last test in debug mode",
+      },
+      {
+        "<leader>td",
+        function()
+          require("neotest").run.run({ strategy = "dap" })
+        end,
+        desc = "Run test in debug mode",
+      },
+      {
+        "<leader>ts",
+        function()
+          require("neotest").summary.toggle()
+        end,
+        desc = "Toggle test summary",
+      },
+      {
+        "<leader>to",
+        function()
+          require("neotest").output.open({ short = true, enter = true })
+        end,
+        desc = "Open test output",
+      },
+    },
+    opts = function()
+      return {
+        adapters = {
+          require("neotest-dotnet")({
+            dap = { justMyCode = false },
+            discovery_root = "project",
+          })
+        },
+        icons = {
+          passed = " ",
+          running = " ",
+          failed = " ",
+          unknown = " ",
+          running_animated = vim.tbl_map(function(s)
+            return s .. " "
+          end, { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" }),
+        },
+        output = {
+          open_on_run = false,
+        },
+      }
+    end,
   },
 }
