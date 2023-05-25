99c2e55eff0a3644b433cf5755a4c13b37cf07ae
joshuasearle <joshryansearle@gmail.com>
2023-05-25 16:02:33 +1000

    Explicitly set newline EOF option

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -102,3 +102,6 @@ opt.showmode = false
 
 local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 vim.env.PATH = vim.env.PATH .. (is_windows and ";" or ":") .. vim.fn.stdpath "data" .. "/mason/bin"
+
+-- Always newline at end of file
+opt.fixendofline = true
